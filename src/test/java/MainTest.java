daf93ac3745114edc40092b1dbc19f7ce1b97ba4
Bill Schofield <bill@billschofield.net>
2022-11-18 12:20:44 -0600

    Clean project template

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MainTest {
+
+	@Test
+	void shouldPass() {
+		assertThat(true).isTrue();
+	}
+
+}
