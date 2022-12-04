0d446353cd6c34af99fa53c0b9cf1cb02c0cb391
Bill Schofield <bill@billschofield.net>
2022-12-04 17:46:48 -0600

    Removed practice implementation of first two stories.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
-		AccountManagement accountManagement = new AccountManagement(System.out);
-		System.out.println("Please enter the new account holder's name");
-		Scanner scanner = new Scanner(System.in);
-		String accountHolderName = scanner.nextLine();
-		CheckingAccount newAccount = accountManagement.submit(accountHolderName);
-		System.out.println("Current balance is $" + newAccount.balance());
+		System.out.println("Hello World!");
 
 	}
 }
